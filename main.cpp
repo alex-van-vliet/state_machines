eb1c0472f2547a237211a8d1c9a1dc146a1cdede
Alex van Vliet <alex.van-vliet@epita.fr>
2023-03-05 15:03:23 +0100

    feat: update mains

--- main.cpp
+++ main.cpp
@@ -1,31 +1,117 @@
+#include <array>
+
 #include "dfa.hpp"
 #include "nfa.hpp"
 #include "regex.hpp"
 #include "runtime_nfa.hpp"
 #include "string.hpp"
 
+#ifdef STAGE6
+#define STAGE5
+#endif
+#ifdef STAGE5
+#define STAGE4
+#endif
+#ifdef STAGE4
+#define STAGE3
+#endif
+#ifdef STAGE3
+#define STAGE2
+#endif
+#ifdef STAGE2
+#define STAGE1
+#endif
+
 using regex::Character, regex::CharacterRange, regex::star;
 
-constexpr auto non_null_digit = Character<'1'>{} | Character<'2'>{} | Character<'3'>{} | Character<'4'>{} | Character<'5'>{} | Character<'6'>{} | Character<'7'>{} | Character<'8'>{} | Character<'9'>{};
-constexpr auto digit = CharacterRange<'0', '9'>{};
-using sm1 = regex::ToStateMachineT<decltype((non_null_digit & star(digit)) | Character<'0'>{})>;
+// FIXME: code below is temporary just to be feature-compatible with the new version
+
+template<size_t N>
+    requires(N >= 1)
+struct StringLiteral {
+    std::array<char, N - 1> value{};
+
+    constexpr StringLiteral(const char (&str)[N]) {
+        std::copy_n(str, N - 1, value.begin());
+    }
+};
+
+template<StringLiteral S, size_t I = 0>
+    requires(S.value.size() > 0)
+struct StringLiteralToUnions {
+    static constexpr auto value = ([] {
+        if constexpr (I == S.value.size() - 1) {
+            return Character<S.value[0]>{};
+        } else {
+            return StringLiteralToUnions<S, I + 1>::value | Character<S.value[S.value.size() - 1 - I]>{};
+        }
+    })();
+};
+
+template<template<auto... Values> typename ResultType, typename ElementType, size_t N, std::array<ElementType, N> Array, size_t... Is>
+auto as_pack_helper(std::integer_sequence<size_t, Is...>) -> ResultType<Array[Is]...>;
+
+template<template<auto... Values> typename ResultType, std::array Array>
+using as_pack_t = decltype(as_pack_helper<ResultType, typename decltype(Array)::value_type, Array.size(), Array>(std::make_index_sequence<Array.size()>{}));
+
+template<StringLiteral S>
+using as_string_t = as_pack_t<::string::String, S.value>;
+
+template<StringLiteral S>
+    requires(S.value.size() > 0)
+constexpr auto operator""_c() {
+    return StringLiteralToUnions<S>::value;
+}
+
+template<StringLiteral S>
+    requires(S.value.size() == 3 && S.value[1] == '-')
+constexpr auto operator""_r() {
+    return CharacterRange<S.value[0], S.value[2]>{};
+}
+
+template<StringLiteral S>
+constexpr auto operator""_s() {
+    return as_string_t<S>{};
+}
+
+#ifdef STAGE1
+using sm1 = regex::ToStateMachineT<decltype(("123456789"_c & star("0-9"_r)) | "0"_c)>;
+#endif
+#ifdef STAGE2
 using sm2 = nfa::EpsilonRemovalT<sm1>;
+#endif
+#ifdef STAGE3
 using sm3 = dfa::DFAConversionT<sm2>;
+#endif
 
 int main() {
-    sm1::draw();
-    sm2::draw();
-    sm3::draw();
-
-#define TEST(sm, s, ...) std::cout << s << ": " << runtime_nfa::ToRuntimeT<sm>{}.test(s) << " - " << dfa::EvaluateV<sm, ::string::String<__VA_ARGS__>> << std::endl;
-    TEST(sm3, "")
-    TEST(sm3, "0", '0')
-    TEST(sm3, "1", '1')
-    TEST(sm3, "01", '0', '1')
-    TEST(sm3, "10", '1', '0')
-    TEST(sm3, "1234567890", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
-    TEST(sm3, "42", '4', '2')
-    TEST(sm3, "042", '0', '4', '2')
-
-    return 0;
+#ifdef STAGE1
+    static_assert(!std::is_same_v<sm1, void>);
+#endif
+#ifdef STAGE2
+    static_assert(!std::is_same_v<sm2, void>);
+#endif
+#ifdef STAGE3
+    static_assert(!std::is_same_v<sm3, void>);
+#endif
+#ifdef STAGE4
+    static_assert(dfa::EvaluateV<sm3, decltype("1234567890"_s)>);
+#endif
+#ifdef STAGE5
+    std::cout << "1234567890"
+              << ": " << 1
+              << " - " << runtime_nfa::ToRuntimeT<sm3>{}.test("1234567890")
+              << " - " << dfa::EvaluateV<sm3, decltype("1234567890"_s)> << std::endl;
+#endif
+#ifdef STAGE6
+#define TEST(sm, expected, s) std::cout << s << ": " << expected << " - " << runtime_nfa::ToRuntimeT<sm>{}.test(s) << " - " << dfa::EvaluateV<sm, decltype(s##_s)> << std::endl;
+    TEST(sm3, 0, "")
+    TEST(sm3, 1, "0")
+    TEST(sm3, 1, "1")
+    TEST(sm3, 0, "01")
+    TEST(sm3, 1, "10")
+    TEST(sm3, 1, "1234567890")
+    TEST(sm3, 1, "42")
+    TEST(sm3, 0, "042")
+#endif
 }
--- main2.cpp
+++ main2.cpp
@@ -1,13 +1,54 @@
 #include "helpers/string.hpp"
 #include "regex/tokens.hpp"
 
+#ifdef STAGE6
+#define STAGE5
+#endif
+#ifdef STAGE5
+#define STAGE4
+#endif
+#ifdef STAGE4
+#define STAGE3
+#endif
+#ifdef STAGE3
+#define STAGE2
+#endif
+#ifdef STAGE2
+#define STAGE1
+#endif
+
 using namespace helpers::literals;
 using namespace regex::literals;
+#ifdef STAGE1
 using sm1 = regex::to_state_machine_t<decltype(("123456789"_c & regex::star("0-9"_r)) | "0"_c)>;
+#endif
+#ifdef STAGE2
 using sm2 = regex::to_nfa_t<sm1>;
+#endif
+#ifdef STAGE3
 using sm3 = regex::to_dfa_t<sm2>;
+#endif
 
 int main() {
+#ifdef STAGE1
+    static_assert(!std::is_same_v<sm1, void>);
+#endif
+#ifdef STAGE2
+    static_assert(!std::is_same_v<sm2, void>);
+#endif
+#ifdef STAGE3
+    static_assert(!std::is_same_v<sm3, void>);
+#endif
+#ifdef STAGE4
+    static_assert(regex::test_v<sm3, "1234567890">);
+#endif
+#ifdef STAGE5
+    std::cout << "1234567890"
+              << ": " << 1
+              << " - " << regex::runtime<sm3>{}.test("1234567890")
+              << " - " << regex::test_v<sm3, "1234567890"> << std::endl;
+#endif
+#ifdef STAGE6
 #define TEST(sm, expected, s) std::cout << s << ": " << expected << " - " << regex::runtime<sm>{}.test(s) << " - " << regex::test_v<sm, s> << std::endl;
     TEST(sm3, 0, "")
     TEST(sm3, 1, "0")
@@ -17,9 +58,5 @@ int main() {
     TEST(sm3, 1, "1234567890")
     TEST(sm3, 1, "42")
     TEST(sm3, 0, "042")
-
-    std::cout << "1234567890"
-              << ": " << 1
-              << " - " << regex::runtime<sm3>{}.test("1234567890")
-              << " - " << regex::test_v<sm3, "1234567890"> << std::endl;
+#endif
 }
